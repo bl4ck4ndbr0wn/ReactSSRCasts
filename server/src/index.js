a843bcd0cb00aa2b64115b224f9b3fe3ebd27789
bl4ck4ndbr0wn <alphandirangu@gmail.com>
2019-02-14 17:37:33 +0300

    Redux: Browser store creation for server

--- server/src/index.js
+++ server/src/index.js
@@ -1,12 +1,18 @@
 import express from "express";
 import renderer from "./helpers/renderer";
+import createStore from "./helpers/createStore";
 
 const app = express();
 
 app.use(express.static("public"));
 
 app.get("*", (req, res) => {
-  res.send(renderer(req));
+  const store = createStore();
+
+  // Logic to initialize
+  // Load data to the store
+
+  res.send(renderer(req, store));
 });
 
 app.listen(process.env.PORT || 3000, () =>
