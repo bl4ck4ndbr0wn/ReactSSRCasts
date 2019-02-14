42836b1fa7e6bb723f34484d41205837dfbc7058
bl4ck4ndbr0wn <alphandirangu@gmail.com>
2019-02-14 19:48:04 +0300

    Mitigating XSS Attacks.

--- server/build/bundle.js
+++ server/build/bundle.js
@@ -199,7 +199,7 @@ var _renderer = __webpack_require__(12);
 
 var _renderer2 = _interopRequireDefault(_renderer);
 
-var _createStore = __webpack_require__(15);
+var _createStore = __webpack_require__(16);
 
 var _createStore2 = _interopRequireDefault(_createStore);
 
@@ -396,6 +396,10 @@ var _reactRedux = __webpack_require__(3);
 
 var _reactRouterConfig = __webpack_require__(1);
 
+var _serializeJavascript = __webpack_require__(15);
+
+var _serializeJavascript2 = _interopRequireDefault(_serializeJavascript);
+
 var _Routes = __webpack_require__(2);
 
 var _Routes2 = _interopRequireDefault(_Routes);
@@ -417,7 +421,7 @@ exports.default = function (req, store) {
     )
   ));
 
-  return "\n  <html>\n    <head></head>\n    <body>\n      <div id=\"root\">" + content + "</div>\n\n      <script>\n      window.INITIAL_STATE = " + JSON.stringify(store.getState()) + "\n      </script>\n      <script src=\"bundle.js\"></script>\n    </body>\n  </html>\n  ";
+  return "\n  <html>\n    <head></head>\n    <body>\n      <div id=\"root\">" + content + "</div>\n\n      <script>\n      window.INITIAL_STATE = " + (0, _serializeJavascript2.default)(store.getState()) + "\n      </script>\n      <script src=\"bundle.js\"></script>\n    </body>\n  </html>\n  ";
 };
 
 /***/ }),
@@ -434,6 +438,12 @@ module.exports = require("react-router-dom");
 
 /***/ }),
 /* 15 */
+/***/ (function(module, exports) {
+
+module.exports = require("serialize-javascript");
+
+/***/ }),
+/* 16 */
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
@@ -445,11 +455,11 @@ Object.defineProperty(exports, "__esModule", {
 
 var _redux = __webpack_require__(5);
 
-var _reduxThunk = __webpack_require__(16);
+var _reduxThunk = __webpack_require__(17);
 
 var _reduxThunk2 = _interopRequireDefault(_reduxThunk);
 
-var _reducers = __webpack_require__(17);
+var _reducers = __webpack_require__(18);
 
 var _reducers2 = _interopRequireDefault(_reducers);
 
@@ -462,13 +472,13 @@ exports.default = function () {
 };
 
 /***/ }),
-/* 16 */
+/* 17 */
 /***/ (function(module, exports) {
 
 module.exports = require("redux-thunk");
 
 /***/ }),
-/* 17 */
+/* 18 */
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
@@ -480,7 +490,7 @@ Object.defineProperty(exports, "__esModule", {
 
 var _redux = __webpack_require__(5);
 
-var _usersReducers = __webpack_require__(18);
+var _usersReducers = __webpack_require__(19);
 
 var _usersReducers2 = _interopRequireDefault(_usersReducers);
 
@@ -491,7 +501,7 @@ exports.default = (0, _redux.combineReducers)({
 });
 
 /***/ }),
-/* 18 */
+/* 19 */
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
